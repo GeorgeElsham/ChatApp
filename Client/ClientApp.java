e506861e27cf4dc96f3a48fb13f47bd05003c4de
GeorgeElsham <40073010+GeorgeElsham@users.noreply.github.com>
2021-11-08 02:16:33 +0000

    Client gets host from arguments, or `localhost` otherwise

--- Client/ClientApp.java
+++ Client/ClientApp.java
@@ -4,12 +4,13 @@ import java.net.ConnectException;
 public class ClientApp {
   public static void main(String[] args) {
     final ClientApp app = new ClientApp();
-    app.run();
+    final String host = args.length > 0 ? args[0] : "localhost";
+    app.run(host);
   }
 
-  public void run() {
+  public void run(String host) {
     try {
-      final Client client = new Client("localhost", new Port(8000));
+      new Client(host, new Port(8000));
     } catch (ConnectException connectException) {
       System.err.println(connectException.getLocalizedMessage());
     } catch (IOException e) {
